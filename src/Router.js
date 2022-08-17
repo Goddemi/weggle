122fa0b003b557a59239877e765cc6b67be396d1
youngsoon12 <enejwk789@gmail.com>
2022-08-17 12:07:43 +0900

    add: router

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Page from './pages/Page';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import OrderList from './pages/OrderList/OrderList';
+import ProductList from './pages/ProductList/ProductList';
+import ProductRegist from './pages/ProductRegist/ProductRegist';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Page />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order-list" element={<OrderList />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product-regist" element={<ProductRegist />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/OrderList/OrderList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrderInquriy = () => {
+  return <div>order</div>;
+};
+
+export default OrderInquriy;
